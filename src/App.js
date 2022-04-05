e694c2be706965b801142578244cd820e87af3a1
siddiq-1018 <siddiqparveen333@gmail.com>
2022-04-05 11:07:53 +0530

    Component Changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './style.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import "swiper/css/bundle";
 import Slider from './components/slider';
+import Offer from './components/offer';
 
 export default function App() {
   return (
@@ -11,38 +12,8 @@ export default function App() {
       <div>
         <Slider />
       </div>
-      <div className="card m-4 p-4 bg-transparent">
-        <h2 className="text-center">
-          Enjoy the best benefits with NESLA savings bank account
-        </h2>
-        <div className="container m-4">
-          <div className="row d-flex justify-content-between">
-            <div className="bg-transparent col-3 p-2">
-              <div className="card card-bg">
-                <h2>8%</h2> <span>interest rate* p.a.</span>
-              </div>
-            </div>
-            <div className=" bg-transparent col-3 p-2">
-              <div className="card card-bg">
-                <h2>₹2Cr</h2> <span>Air Accidental Insurance Cover</span>
-              </div>
-            </div>
-            <div className="bg-transparent col-3 p-2">
-              <div className="card card-bg">
-                <h2>₹40L</h2> <span>Personal Accidental Insurance Cover</span>
-              </div>
-            </div>
-            <div className="bg-transparent col-3 p-2">
-              <div className="card card-bg">
-                <h2>₹500</h2>
-                <span>Cashback on your first debit card transaction*</span>
-              </div>
-            </div>
-            <div className="mt-4 p-2 text-center">
-              <button className="btn btn-primary">Know more..</button>
-            </div>
-          </div>
-        </div>
+      <div>
+        <Offer />
       </div>
     </div>
   );
